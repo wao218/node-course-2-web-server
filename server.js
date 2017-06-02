c2918e1253ec018d1b009d1f26b691d546b6a6c5
Wesley Osborne <wao218@lehigh.edu>
2017-06-02 10:31:23 -0400

    Add project page

--- server.js
+++ server.js
@@ -58,6 +58,14 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  // res.send('About Page');
+  res.render('projects.hbs', {
+    pageTitle: 'Projects Page',
+
+  });
+});
+
 // /bd - send back json with errorMessage
 app.get('/bad', (req, res) => {
   res.send({
